69a35b70425733b5dc8b24a4fbcfab34eeac690a
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2021-11-13 17:04:21 -0600

    4

--- 08-cuda/example4.cu
+++ 08-cuda/example4.cu
@@ -16,62 +16,78 @@
 #define THREADS	256
 #define BLOCKS	MMIN(32, ((SIZE / THREADS) + 1))
 // implement your code
-__global__ void even(int *array, int *results) {
+__global__ void sum(int *array, long *result) {
+	__shared__ long cache[THREADS];
 
 	int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+	int cacheIndex = threadIdx.x;
 
+	long acum = 0;
 	while (tid < SIZE) {
-		//aux = (aux < array[tid])? aux : array[tid];
     if((array[tid]%2)==0){
-      *results ++;
+      acum ++;
     }
 		tid += blockDim.x * gridDim.x;
 	}
 
+	cache[cacheIndex] = acum;
+
+	__syncthreads();
+
+	int i = blockDim.x / 2;
+	while (i > 0) {
+		if (cacheIndex < i) {
+			cache[cacheIndex] += cache[cacheIndex + i];
+		}
+		__syncthreads();
+		i /= 2;
+	}
+
+	if (cacheIndex == 0) {
+		result[blockIdx.x] = cache[cacheIndex];
+	}
 }
 
 int main(int argc, char* argv[]) {
-	int i, *a, *results;
-  int *d_a, *d_r;
+	int i, *array, *d_a;
+	long *results, *d_r;
 	double ms;
 
-	a = (int *) malloc(sizeof(int) * SIZE);
-	fill_array(a, SIZE);
-	display_array("a", a);
+	array = (int*) malloc( SIZE * sizeof(int) );
+	fill_array(array, SIZE);
+	display_array("array", array);
 
-  //results = (int *) malloc(sizeof(int) * BLOCKS);
-  results = 0;
+	results = (long*) malloc( BLOCKS * sizeof(long) );
 
 	cudaMalloc( (void**) &d_a, SIZE * sizeof(int) );
-	cudaMalloc( (void**) &d_r, BLOCKS * sizeof(int) );
+	cudaMalloc( (void**) &d_r, BLOCKS * sizeof(long) );
 
-	cudaMemcpy(d_a, a, SIZE * sizeof(int), cudaMemcpyHostToDevice);
-  //cudaMemcpy(d_r, results, SIZE * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_a, array, SIZE * sizeof(int), cudaMemcpyHostToDevice);
 
 	printf("Starting...\n");
 	ms = 0;
 	for (i = 1; i <= N; i++) {
 		start_timer();
 
-		even<<<BLOCKS, THREADS>>> (d_a, d_r);
+		sum<<<BLOCKS, THREADS>>> (d_a, d_r);
 
 		ms += stop_timer();
 	}
 
-	cudaMemcpy(results, d_r, BLOCKS * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(results, d_r, BLOCKS * sizeof(long), cudaMemcpyDeviceToHost);
 
-	/*int aux = INT_MAX;
+	long acum = 0;
 	for (i = 0; i < BLOCKS; i++) {
-		aux = MMIN(aux, results[i]);
-	}*/
+		acum += results[i];
+	}
 
-	printf("result = %i\n", results);
+	printf("sum = %li\n", acum);
 	printf("avg time = %.5lf\n", (ms / N));
 
 	cudaFree(d_r);
 	cudaFree(d_a);
 
-	free(a);
-  free(results);
+	free(array);
+	free(results);
 	return 0;
 }
