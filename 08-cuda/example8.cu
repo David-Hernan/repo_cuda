131d56f4a0d3789e45bd1b1ed4a40285d167e315
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2021-11-14 01:50:17 -0600

    test

--- 08-cuda/example8.cu
+++ 08-cuda/example8.cu
@@ -34,64 +34,58 @@
 #define THREADS 256
 #define BLOCKS	MMIN(32, ((SIZE / THREADS) + 1))
 
-__global__ void even(int* arr, int size) {
-  int i, aux;
+__global__ void even(int* arr, int * c, int size) {
+  int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+  int pos;
 
-  i = (threadIdx.x * 2);
-  if (i <= size - 2) {
-    if (arr[i] > arr[i + 1]) {
-      aux = arr[i];
-      arr[i] = arr[i + 1];
-      arr[i + 1] = aux;
+  while (tid < SIZE){
+    pos=0;
+    for(int i=0; i<size; i++){
+      if(arr[i] < arr[tid] || arr[i] == arr[tid] && i < tid){
+        pos++;
+      }
     }
+    c[pos]=arr[tid];
   }
-}
-
-__global__ void odd(int* arr, int size) {
-  int i, aux;
-
-  i = (threadIdx.x * 2) + 1;
-  if (i <= size - 2) {
-    if (arr[i] > arr[i + 1]) {
-      aux = arr[i];
-      arr[i] = arr[i + 1];
-      arr[i + 1] = aux;
-    }
+  while (tid < SIZE){
+    arr[tid]=c[tid];
   }
 }
 
 int main(int argc, char* argv[]) {
-	int i, j, *a, *d_a;
+	int i, j, *a, *d_a, *c, *d_c;
 	double ms;
 
 	a = (int *) malloc(sizeof(int) * SIZE);
 	random_array(a, SIZE);
 	display_array("a", a);
+  c = (int *) malloc(sizeof(int) * SIZE);
 
 	cudaMalloc( (void**) &d_a, SIZE * sizeof(int) );
+  cudaMalloc( (void**) &d_c, SIZE * sizeof(int) );
 
   printf("Starting...\n");
 	ms = 0;
+  cudaMemcpy(d_a, a, SIZE * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_c, c, SIZE * sizeof(int), cudaMemcpyHostToDevice);
+
 	for (i = 1; i <= N; i++) {
-    cudaMemcpy(d_a, a, SIZE * sizeof(int), cudaMemcpyHostToDevice);
-
 		start_timer();
-
     for (j = 0; j <= SIZE / 2; j++) {
-      even<<<1, THREADS>>>(d_a, SIZE);
-      odd<<<1, THREADS>>>(d_a,SIZE);
+      even<<<1, THREADS>>>(d_a, d_c, SIZE);
     }
-
 		ms += stop_timer();
 	}
 
-  cudaMemcpy(a, d_a, SIZE * sizeof(int), cudaMemcpyDeviceToHost);
-	display_array("a", a);
+  cudaMemcpy(c, d_c, SIZE * sizeof(int), cudaMemcpyDeviceToHost);
+	display_array("c", c);
   printf("avg time = %.5lf ms\n", (ms / N));
 
 	cudaFree(d_a);
+  cudaFree(d_c);
 
 	free(a);
+  free(c);
 
   return 0;
 }
