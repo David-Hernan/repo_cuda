f655a35a6c9530bf16f06ea46cd0ec612549b30d
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2021-11-14 00:06:01 -0600

    test

--- 08-cuda/test.cu
+++ 08-cuda/test.cu
@@ -19,49 +19,54 @@
 
 #define MAXIMUM 1000000 //1e6
 #define THREADS 256
-#define BLOCKS	MMIN(32, ((SIZE / THREADS) + 1))
+#define BLOCKS	MMIN(32, ((MAXIMUM / THREADS) + 1))
 
-__global__ void GetPrime(int *array, long *pos) {
-	__shared__ long cache[THREADS];
+__global__ void GetPrime(int *array, int pos) {
 
 	int tid = threadIdx.x + (blockIdx.x * blockDim.x);
-	int cacheIndex = threadIdx.x;
 
-	int aux, b, i, j;
+	int aux, b, i;
 	while (tid < MAXIMUM) {
-        array[tid]=1;
-        aux=1;
-        for(i = 2; i <= sqrt((float)tid); i++){
-				// if(arr[i-1] % i == 0){
-				// 	pos[i]=0;
-				// }else{
-				// 	pos[i]=1;
-				// }
-                if(!(tid%i)){
-                    aux=0;
-                }
+        // array[tid]=1;
+        // aux=1;
+        // for(i = 2; i <= sqrt((float)tid); i++){
+        //         if(!(tid%i)){
+        //             aux=0;
+        //         }
+		// 	}
+        //     array[tid]=aux;
+        //     tid += blockDim.x * gridDim.x;
+        // }
+
+			b=i/2;
+			if(b<2){
+				b=2;
 			}
-            array[tid]=aux;
+			aux=0;
+			for(i = 2; i <= b; i++){
+				if(array[tid-1] % i == 0){
+					aux=0;
+				}else{
+					aux=1;
+				}
+			}
+			array[tid]=aux;
             tid += blockDim.x * gridDim.x;
-        }
 	}
+}
 
 int main(int argc, char* argv[]) {
-	int i, *a, *aux,  *d_a;
+	int i, *a, *d_a;
 	double ms;
 
-	a = (int *) malloc(sizeof(int) * MAXIMUM);
-    // display_array("a", a);
-	for (i = 2; i < TOP_VALUE; i++) {
-		if(a[i] == 0){
-            printf("%i ",i)
-        }
-	}
+	a = (int*) malloc(sizeof(int) * MAXIMUM);
+	fill_array(a, MAXIMUM);
+	display_array("before", a);
 
     cudaMalloc( (void**) &d_a, MAXIMUM * sizeof(int) );
     cudaMemcpy(d_a, a, MAXIMUM * sizeof(int), cudaMemcpyHostToDevice);
 
-    printf("Starting...\n");
+    printf("\nStarting...\n");
 	ms = 0;
 	for (i = 1; i <= N; i++) {
 		start_timer();
@@ -80,7 +85,7 @@ int main(int argc, char* argv[]) {
         }
 	}
 
-    printf("avg time = %.5lf ms\n", (ms / N));
+    printf("\navg time = %.5lf ms\n", (ms / N));
 
     cudaFree(d_a);
 	free(a);
