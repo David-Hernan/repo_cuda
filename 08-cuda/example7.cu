7635e85025c99e9128a540c66575f349834c5eac
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2021-11-13 19:22:03 -0600

    5

--- 08-cuda/example7.cu
+++ 08-cuda/example7.cu
@@ -19,4 +19,63 @@
 
 #define MAXIMUM 1000000 //1e6
 #define THREADS 256
-#define BLOCKS	MMIN(32, ((SIZE / THREADS) + 1))
+//#define BLOCKS	MMIN(32, ((SIZE / THREADS) + 1))
+#define BLOCKS	MMIN(32, ((MAXIMUM / THREADS) + 1))
+// implement your code
+__global__ void even(int* arr, int size) {
+  int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+
+  int j, prime;
+
+  while (tid < MAXIMUM) {
+    if(tid>=2){
+      j=2;
+			prime=1;
+			while(j<tid){
+				if(tid%j == 0){
+					prime=0;
+					arr[tid]=0;
+					break;
+				}
+				j++;
+			}
+			if(prime == 1){
+				arr[tid]=1;
+			}
+    }
+		tid += blockDim.x * gridDim.x;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	int i, j, *a, *d_a;
+	double ms;
+
+	a = new int[MAXIMUM + 1];
+
+	cudaMalloc( (void**) &d_a, MAXIMUM * sizeof(int) );
+
+  printf("Starting...\n");
+	ms = 0;
+	for (i = 1; i <= N; i++) {
+    cudaMemcpy(d_a, a, MAXIMUM * sizeof(int), cudaMemcpyHostToDevice);
+
+		start_timer();
+
+    for (j = 0; j <= MAXIMUM / 2; j++) {
+      even<<<BLOCKS, THREADS>>>(d_a, MAXIMUM);
+    }
+
+		ms += stop_timer();
+	}
+
+  cudaMemcpy(a, d_a, MAXIMUM * sizeof(int), cudaMemcpyDeviceToHost);
+	display_array("a", a);
+  printf("avg time = %.5lf ms\n", (ms / N));
+
+	cudaFree(d_a);
+
+	free(a);
+
+  return 0;
+}
